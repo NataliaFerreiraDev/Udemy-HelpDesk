dbda228cb30c4ab61b699eb56cb7bd3d27b3d74f
NataliaFerreiraDev <nataliaferreira.de@yahoo.com>
2021-11-11 13:46:18 -0300

    first commit

---
+++ src/test/java/br/com/udemy/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
